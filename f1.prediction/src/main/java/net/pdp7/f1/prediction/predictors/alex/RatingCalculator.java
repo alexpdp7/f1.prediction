f1c1278715fe89f6ce2014fce077b51df91a1dea
alex <alex@pdp7.net>
2011-11-25 04:35:20 +0100

    add calculateTeamRating, comments

--- f1.prediction/src/main/java/net/pdp7/f1/prediction/predictors/alex/RatingCalculator.java
+++ f1.prediction/src/main/java/net/pdp7/f1/prediction/predictors/alex/RatingCalculator.java
@@ -1,5 +1,8 @@
 package net.pdp7.f1.prediction.predictors.alex;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
 public class RatingCalculator {
@@ -10,8 +13,9 @@ public class RatingCalculator {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 	
+	/** @return drivers who finished below driverName / total drivers who finished - 1, or 0 if driver did not finish */
 	public float calculateDriverRating(int season, int round, String driverName) {
-		if(jdbcTemplate.queryForInt("select count(*) from grand_prix_driver_results where season = ? and round = ? and finish_position is not null and driver_name = ?", season, round, driverName) == 0) {
+		if(!didDriverFinish(season, round, driverName)) {
 			return 0;
 		}
 		
@@ -19,6 +23,34 @@ public class RatingCalculator {
 		int finishPosition = jdbcTemplate.queryForInt("select finish_position from grand_prix_driver_results where season = ? and round = ? and driver_name = ?", season, round, driverName);
 		
 		return ((float) (finishers - finishPosition))/(finishers - 1);
+	}
+
+	protected boolean didDriverFinish(int season, int round, String driverName) {
+		return jdbcTemplate.queryForInt("select count(*) from grand_prix_driver_results where season = ? and round = ? and finish_position is not null and driver_name = ?", season, round, driverName) == 1;
+	}
+
+	/** average driver rating of the team's drivers who finished, or 0 if no drivers finished */
+	public float calculateTeamRating(int season, int round, String teamName) {
+		List<String> teamDrivers = jdbcTemplate.query(
+				"select grand_prix_driver_results.driver_name " +
+				"from   grand_prix_driver_results " +
+				"join   season_team_drivers on  grand_prix_driver_results.season = season_team_drivers.season " +
+				"                           and grand_prix_driver_results.driver_name = season_team_drivers.driver_name " +
+				"where  grand_prix_driver_results.season = ? " +
+				"and    round = ? " +
+				"and    team_name = ?", new SingleColumnRowMapper<String>(String.class), season, round, teamName);
+		
+		float totalRating = 0;
+		int finishingDrivers = 0;
+		
+		for(String driverName : teamDrivers) {
+			if(didDriverFinish(season, round, driverName)) {
+				totalRating += calculateDriverRating(season, round, driverName);
+				finishingDrivers++;
+			}
+		}
+		
+		return finishingDrivers == 0 ? 0 : totalRating/finishingDrivers;
 	}
 	
 }
--- f1.prediction/src/test/java/net/pdp7/f1/prediction/predictors/alex/RatingCalculatorTest.java
+++ f1.prediction/src/test/java/net/pdp7/f1/prediction/predictors/alex/RatingCalculatorTest.java
@@ -16,6 +16,9 @@ public class RatingCalculatorTest extends TestCase {
 		assertEquals(0.0, calculator.calculateDriverRating(2011, 5, "Felipe Massa"), 0.001);       // retired
 		assertEquals(12.0/20.0, calculator.calculateDriverRating(2011, 5, "Sergio Pérez"), 0.001); // 9 of 21
 		
+		assertEquals(0.0, calculator.calculateTeamRating(2011, 12, "Toro Rosso"), 0.001);              // both Toro Rosso did not finish
+		assertEquals((1+17.0/18)/2, calculator.calculateTeamRating(2011, 12, "Red Bull"), 0.001);      // 1 and 2 of 19
+		assertEquals((15.0/18+11.0/18)/2, calculator.calculateTeamRating(2011, 12, "Ferrari"), 0.001); // 4 and 8 of 19
 	}
 	
 }
