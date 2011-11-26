b7ae8a74f4e13b6f7af3f413e4d46c33b29570c0
alex <alex@pdp7.net>
2011-11-26 11:07:56 +0100

    graphical display of evolution

--- f1.prediction/src/main/java/net/pdp7/f1/prediction/predictors/alex/genetic/GeneticPredictorEvolver.java
+++ f1.prediction/src/main/java/net/pdp7/f1/prediction/predictors/alex/genetic/GeneticPredictorEvolver.java
@@ -11,6 +11,7 @@ import net.pdp7.f1.prediction.spring.F1PredictionConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.uncommons.watchmaker.framework.EvolutionEngine;
 import org.uncommons.watchmaker.framework.termination.ElapsedTime;
+import org.uncommons.watchmaker.swing.evolutionmonitor.EvolutionMonitor;
 
 public class GeneticPredictorEvolver {
 
@@ -23,8 +24,12 @@ public class GeneticPredictorEvolver {
 		
 		@SuppressWarnings("unchecked")
 		EvolutionEngine<double[]> evolutionEngine = applicationContext.getBean("evolutionEngine", EvolutionEngine.class);
+		EvolutionMonitor<double[]> evolutionMonitor = new EvolutionMonitor<double[]>();
+		evolutionEngine.addEvolutionObserver(evolutionMonitor);
 		
-		double[] evolve = evolutionEngine.evolve(1000, 10, new ElapsedTime(60000));
+		evolutionMonitor.showInFrame("Evolution", true);
+		
+		double[] evolve = evolutionEngine.evolve(100, 5, new ElapsedTime(1200000));
 		
 		System.out.println(Arrays.toString(evolve));
 		
